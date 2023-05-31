a7a0a7a9bc57e0bdf731a19f2b86aeadce6df50d
Phodal Huang <h@phodal.com>
2023-05-31 16:11:52 +0800

    feat: add for proejcts

--- comate-gui/src/jvmMain/kotlin/Main.kt
+++ comate-gui/src/jvmMain/kotlin/Main.kt
@@ -8,10 +8,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
-import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.PreferenceDataStoreFactory
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.core.screen.uniqueScreenKey
