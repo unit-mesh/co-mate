fdc095b2bfa13c173a2d866b53288205a4e449e1
Phodal Huang <h@phodal.com>
2023-06-01 17:22:38 +0800

    docs: make prompt better

--- comate-cli/src/main/kotlin/org/archguard/copilot/Main.kt
+++ comate-cli/src/main/kotlin/org/archguard/copilot/Main.kt
@@ -28,12 +28,13 @@ fun main() {
     println("""根据下面的 dependencies 信息，编写这个项目的介绍。要求如下：
 
 1. 只返回最可能的 channel type，不做解释。
-2. 根据 dependencies 分析这个应用要考虑的非功能需求。
-3. 根据 dependencies 分析这个应用使用的三个核心库。
+2. 根据 dependencies 分析这个应用的核心场景。
+3. 根据 dependencies 分析这个应用要考虑的功能需求和非功能需求。
 4. 你返回的介绍类似于：
 
 ```
-这个项目是一个 {channel type} 应用程序，使用了 {xxx} 和一系列相关的库来构建 {xxx} 等功能。该应用还使用了一些第三方库来显 {xxx}，以及进行 {xxx} 等任务。该应用需要考虑 {xxx} 等非功能需求。
+这个项目是一个 {channel type} 应用程序，使用了 Jetpack Compose、{xxx} 和一系列相关的库来构建 {xxx} 等功能。
+该应用还使用了一些第三方库来构建用户界面 {xxx}，以及进行 {xxx} 等任务。该应用需要考虑 {xxx} 等非功能需求。
 ```
 
 dependencies: $depNames
