318a58a4a9689277a1796a56d2b75dde0c9f9e88
Phodal Huang <h@phodal.com>
2023-06-02 11:02:04 +0800

    docs: update for cli

--- comate-cli/build.gradle.kts
+++ comate-cli/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     application
     alias(libs.plugins.jvm)
     alias(libs.plugins.serialization)
+    alias(libs.plugins.shadow)
 }
 
 dependencies {
@@ -18,4 +19,18 @@ dependencies {
 
 application {
     mainClass.set("org.archguard.copilot.MainKt")
+}
+
+tasks {
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", "org.archguard.copilot.MainKt"))
+        }
+        // minimize()
+        dependencies {
+            exclude(dependency("org.junit.jupiter:.*:.*"))
+            exclude(dependency("org.junit:.*:.*"))
+            exclude(dependency("junit:.*:.*"))
+        }
+    }
 }
