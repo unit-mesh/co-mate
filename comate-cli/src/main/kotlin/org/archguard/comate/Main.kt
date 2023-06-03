f2cfdd18f73ecd6e59955832bf65f031b53def80
Phodal Huang <h@phodal.com>
2023-06-03 21:00:26 +0800

    refactor: clean codes

--- comate-cli/src/main/kotlin/org/archguard/comate/Main.kt
+++ comate-cli/src/main/kotlin/org/archguard/comate/Main.kt
@@ -1,13 +1,16 @@
 package org.archguard.comate
 
 import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer
-import ai.djl.ndarray.NDManager
+import ai.onnxruntime.OnnxTensor
+import ai.onnxruntime.OrtEnvironment
+import ai.onnxruntime.OrtSession.SessionOptions
 import org.archguard.comate.action.IntroductionPrompt
 import org.archguard.comate.strategy.BasicPromptStrategy
 import java.io.File
 import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.Path
+
 
 fun main(args: Array<String>) {
     val basepath = Path(File(".").absolutePath)
@@ -30,11 +33,14 @@ fun main(args: Array<String>) {
 private fun embed(tokenizer: HuggingFaceTokenizer, sequence: String) {
     val tokenized = tokenizer.encode(sequence, true)
 
-    val manager = NDManager.newBaseManager()
+    val inputIds = tokenized.ids
+    val attentionMask = tokenized.attentionMask
+    val typeIds = tokenized.typeIds
 
-    val inputIdsArray = manager.create(tokenized.ids)
-    val attentionMaskArray = manager.create(tokenized.attentionMask)
-    val tokenTypeIdsArray = manager.create(tokenized.typeIds)
+//    val manager = NDManager.newBaseManager()
+//    val inputIdsArray = manager.create(inputIds)
+//    val attentionMaskArray = manager.create(attentionMask)
+//    val tokenTypeIdsArray = manager.create(typeIds)
 
 //        let outputs = self.session.run([
 //            InputTensor::from_array(inputs_ids_array.into_dyn()),
@@ -42,6 +48,26 @@ private fun embed(tokenizer: HuggingFaceTokenizer, sequence: String) {
 //            InputTensor::from_array(token_type_ids_array.into_dyn()),
 //        ])?;
 
+//    val model = OnnxInferenceModel("model/model.onnx")
+//    val detections = model.inferAndCloseUsing(ExecutionProvider.CPU()) {
+//    }
+
+
+    val env = OrtEnvironment.getEnvironment()
+    val modelPath: String = Path("model/model.onnx").toString()
+    SessionOptions().use { options ->
+//        val a = OnnxTensor.createTensor(env, floatArrayOf(2.0f))
+        env.createSession(modelPath, options).use { session ->
+            val output = session.run(
+                mapOf(
+                    "input_ids" to OnnxTensor.createTensor(env, inputIds),
+                    "attention_mask" to OnnxTensor.createTensor(env, attentionMask),
+                    "token_type_ids" to OnnxTensor.createTensor(env, typeIds),
+                ),
+            )
+            println(output[0])
+        }
+    }
 }
 
 private fun processCmds(args: Array<String>, basepath: Path): String {
