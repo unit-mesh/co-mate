bd640668f0a805d70947c979c305c1e07ca0c4f0
Phodal Huang <h@phodal.com>
2023-06-03 23:01:20 +0800

    refactor: extract fo files

--- comate-cli/src/main/kotlin/org/archguard/comate/Semantic.kt
+++ comate-cli/src/main/kotlin/org/archguard/comate/Semantic.kt
@@ -4,42 +4,9 @@ import ai.djl.huggingface.tokenizers.HuggingFaceTokenizer
 import ai.onnxruntime.OnnxTensor
 import ai.onnxruntime.OrtEnvironment
 import ai.onnxruntime.OrtSession
-import ai.onnxruntime.OrtSession.SessionOptions
 import ai.onnxruntime.OrtUtil
-import org.archguard.comate.action.IntroductionPrompt
-import org.archguard.comate.strategy.BasicPromptStrategy
-import java.io.File
-import java.nio.file.Path
 import java.nio.file.Paths
 import kotlin.io.path.Path
-import kotlin.math.sqrt
-
-
-fun main(args: Array<String>) {
-    val basepath = Path(File(".").absolutePath)
-
-    val create = Semantic.create()
-    val first = create.embed("analysis system")
-    val second = create.embed("analysys systems")
-
-    val similarity = cosineSimilarity(first, second)
-    println(similarity)
-}
-
-fun cosineSimilarity(first: FloatArray, second: FloatArray): Float {
-    var dotProduct = 0f
-    var normA = 0f
-    var normB = 0f
-
-    for (i in first.indices) {
-        dotProduct += first[i] * second[i]
-        normA += first[i] * first[i]
-        normB += second[i] * second[i]
-    }
-
-    val denominator = sqrt(normA) * sqrt(normB)
-    return if (denominator == 0f) 0f else dotProduct / denominator
-}
 
 class Semantic(val tokenizer: HuggingFaceTokenizer, val session: OrtSession, val env: OrtEnvironment) {
     fun embed(
@@ -75,35 +42,10 @@ class Semantic(val tokenizer: HuggingFaceTokenizer, val session: OrtSession, val
             val tokenizer = HuggingFaceTokenizer.newInstance(Paths.get("model/tokenizer.json"))
             val env = OrtEnvironment.getEnvironment()
             val modelPath: String = Path("model/model.onnx").toString()
-            val sessionOptions = SessionOptions()
+            val sessionOptions = OrtSession.SessionOptions()
             val session = env.createSession(modelPath, sessionOptions)
 
             return Semantic(tokenizer, session, env)
         }
     }
-}
-
-private fun processCmds(args: Array<String>, basepath: Path): String {
-    val cmd = if (args.isEmpty()) {
-        "intro"
-    } else {
-        args[0]
-    }
-
-    val command = ComateCommand.valueOf(cmd.capitalize())
-    val prompt = command.prompt(basepath)
-    return prompt
-}
-
-enum class ComateCommand(command: String) {
-    Intro("intro") {
-        override fun prompt(basepath: Path): String {
-            val promptStrategy = BasicPromptStrategy()
-            val basicPrompter = IntroductionPrompt(basepath, promptStrategy)
-            val prompt = basicPrompter.prompt()
-            return prompt
-        }
-    };
-
-    abstract fun prompt(basepath: Path): String
 }
