c06f6c284aa473ed39a8cd5d0de7a68a88ca707c
Phodal Huang <h@phodal.com>
2023-06-06 22:36:19 +0800

    feat: use custom render

--- spec-partitioner/src/main/kotlin/org/archguard/spec/markdown/MarkdownParser.kt
+++ spec-partitioner/src/main/kotlin/org/archguard/spec/markdown/MarkdownParser.kt
@@ -1,11 +1,13 @@
 package org.archguard.spec.markdown
 
-import org.commonmark.Extension
-import org.commonmark.internal.renderer.NodeRendererMap
-import org.commonmark.node.Node
+import org.commonmark.node.*
 import org.commonmark.parser.Parser
-import org.commonmark.renderer.Renderer
-import org.commonmark.renderer.text.*
+import org.commonmark.renderer.NodeRenderer
+import org.commonmark.renderer.text.CoreTextContentNodeRenderer
+import org.commonmark.renderer.text.TextContentNodeRendererContext
+import org.commonmark.renderer.text.TextContentNodeRendererFactory
+import org.commonmark.renderer.text.TextContentRenderer
+import java.util.*
 
 
 class ContentBlocking(val subTitle: String, val content: String)
@@ -16,97 +18,25 @@ class MarkdownParser {
         val parser = Parser.builder().build()
         val node: Node = parser.parse(markdown)
 
-        val text = CustomContentRenderer.builder().build().render(node)
+        val text = TextContentRenderer.builder()
+            .nodeRendererFactory(CustomRenderFactory())
+            .build().render(node)
+
         println(text)
+
         return contentBlocks
     }
 }
 
-class CustomContentRenderer private constructor(builder: Builder) : Renderer {
-    private val stripNewlines: Boolean
-    private val nodeRendererFactories: MutableList<TextContentNodeRendererFactory>
-
-    init {
-        stripNewlines = builder.stripNewlines
-        nodeRendererFactories = java.util.ArrayList(builder.nodeRendererFactories.size + 1)
-        nodeRendererFactories.addAll(builder.nodeRendererFactories)
-        // Add as last. This means clients can override the rendering of core nodes if they want.
-        nodeRendererFactories.add(TextContentNodeRendererFactory { context -> CoreTextContentNodeRenderer(context) })
+class CustomRenderFactory : TextContentNodeRendererFactory {
+    override fun create(context: TextContentNodeRendererContext?): NodeRenderer {
+        return CustomNodeRenderer(context!!)
     }
+}
 
-    override fun render(node: Node, output: Appendable) {
-        val context =
-            RendererContext(TextContentWriter(output))
-        context.render(node)
-    }
-
-    override fun render(node: Node): String {
-        val sb = StringBuilder()
-        render(node, sb)
-        return sb.toString()
-    }
-
-    class Builder {
-        var stripNewlines = false
-        val nodeRendererFactories: MutableList<TextContentNodeRendererFactory> = java.util.ArrayList()
-
-        fun build(): CustomContentRenderer {
-            return CustomContentRenderer(this)
-        }
-
-        fun stripNewlines(stripNewlines: Boolean): Builder {
-            this.stripNewlines = stripNewlines
-            return this
-        }
-
-        fun nodeRendererFactory(nodeRendererFactory: TextContentNodeRendererFactory): Builder {
-            nodeRendererFactories.add(nodeRendererFactory)
-            return this
-        }
-
-        fun extensions(extensions: Iterable<Extension?>): Builder {
-            for (extension in extensions) {
-                if (extension is TextContentRendererExtension) {
-                    extension.extend(this)
-                }
-            }
-            return this
-        }
-    }
-
-    interface TextContentRendererExtension : Extension {
-        fun extend(rendererBuilder: Builder?)
-    }
-
-    private inner class RendererContext(private val textContentWriter: TextContentWriter) :
-        TextContentNodeRendererContext {
-        private val nodeRendererMap = NodeRendererMap()
-
-        init {
-            // The first node renderer for a node type "wins".
-            for (i in nodeRendererFactories.indices.reversed()) {
-                val nodeRendererFactory = nodeRendererFactories[i]
-                val nodeRenderer = nodeRendererFactory.create(this)
-                nodeRendererMap.add(nodeRenderer)
-            }
-        }
-
-        override fun stripNewlines(): Boolean {
-            return stripNewlines
-        }
-
-        override fun getWriter(): TextContentWriter {
-            return textContentWriter
-        }
-
-        override fun render(node: Node) {
-            nodeRendererMap.render(node)
-        }
-    }
-
-    companion object {
-        fun builder(): Builder {
-            return Builder()
-        }
+class CustomNodeRenderer(context: TextContentNodeRendererContext) : CoreTextContentNodeRenderer(context) {
+    override fun render(node: Node?) {
+        println("rendering node: $node")
+        node?.accept(this)
     }
 }
