92a55e88cb5d0ef252173a7ef9f3b8aa7b993475
Phodal Huang <h@phodal.com>
2023-06-07 22:21:34 +0800

    docs: try to set security rules

--- meta-action/src/main/kotlin/org/archguard/meta/restful/ApiRule.kt
+++ meta-action/src/main/kotlin/org/archguard/meta/restful/ApiRule.kt
@@ -5,11 +5,12 @@ import org.archguard.meta.AtomicDsl
 import org.archguard.meta.LlmRuleVerifier
 
 @AtomicDsl
-abstract class ApiRule(val name: String, var rule: String) : AtomicAction {
+abstract class ApiRule(open val name: String, open var rule: String) : AtomicAction {
     abstract fun exec(input: RestApi): Any
 }
 
-abstract class ApiCheckRule(name: String, rule: String, open var ruleVerifier: LlmRuleVerifier) : ApiRule(name, rule) {
+abstract class ApiCheckRule(override val name: String, override var rule: String, open var ruleVerifier: LlmRuleVerifier) :
+    ApiRule(name, rule) {
     override fun exec(input: RestApi): Boolean {
         return exec(input)
     }
