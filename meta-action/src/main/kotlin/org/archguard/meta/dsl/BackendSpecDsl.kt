4ac57cd0fa35897e6a47954364f312d1e20bd785
Phodal Huang <h@phodal.com>
2023-06-12 10:02:15 +0800

    docs: init for doc spec

---
+++ meta-action/src/main/kotlin/org/archguard/meta/dsl/BackendSpecDsl.kt
@@ -0,0 +1,32 @@
+package org.archguard.meta.dsl
+
+class BackendSpecDsl {
+    fun repository_name(name: String) {
+
+    }
+
+    fun layered_style(style: String) {
+
+    }
+
+    fun code_style(style: String) {
+//    fun class_name_style(style: String) {
+//
+//    }
+//
+//    fun method_name_style(style: String) {
+//
+//    }
+//
+//    fun variable_name_style(style: String) {
+//
+//    }
+    }
+    fun exception(style: String) {
+
+    }
+
+    fun security(style: String) {
+
+    }
+}
