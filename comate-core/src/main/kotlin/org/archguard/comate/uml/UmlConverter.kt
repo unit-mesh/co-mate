7248b78cdb3020565781a94196942d313a9220fc
Phodal Huang <h@phodal.com>
2023-06-13 13:38:46 +0800

    refactor: extract for block

--- comate-core/src/main/kotlin/org/archguard/comate/uml/UmlConverter.kt
+++ comate-core/src/main/kotlin/org/archguard/comate/uml/UmlConverter.kt
@@ -1,15 +1,19 @@
 package org.archguard.comate.uml
 
 import chapi.domain.core.CodeDataStruct
+import chapi.domain.core.CodeFunction
 
 class UmlConverter {
     fun fromChapi(element: CodeDataStruct): String {
-        return """
-            @startuml
-            class ${element.NodeName} {
-                ${element.Functions.joinToString("\n") { "${it.ReturnType} ${it.Name}()" }}
-            }
-            @enduml
-        """.trimIndent()
+        val classBlock = """class ${element.NodeName} {
+                ${functionBlock(element.Functions)}
+            }"""
+
+        return wrapper(classBlock)
     }
+
+    private fun functionBlock(codeFunctions: List<CodeFunction>) =
+        codeFunctions.joinToString("\n") { "${it.ReturnType} ${it.Name}()" }
+
+    private fun wrapper(classBlock: String) = "@startuml\n$classBlock\n@enduml"
 }
