7586540e121498dfd60668621e1936edeb2cdda7
Phodal Huang <h@phodal.com>
2023-06-13 15:11:09 +0800

    refactor: update for format

--- comate-core/src/main/kotlin/org/archguard/comate/action/LayeredStylePrompt.kt
+++ comate-core/src/main/kotlin/org/archguard/comate/action/LayeredStylePrompt.kt
@@ -8,8 +8,24 @@ class LayeredStylePrompt(
     val context: CommandContext,
     override val strategy: Strategy,
 ) : CodePromptStrategy {
-    override fun getRole(): String = "Architecture"
-    override fun getInstruction(): String = "根据下面的信息，分析项目的分层是否符合业内的通用规范？并绘制 PlantUML 图来表示。"
+    override fun getRole(): String = "Software Architecture"
+    override fun getInstruction(): String = "根据下面的信息，分析项目的分层是否符合业内的通用规范？并绘制 Graphviz 图来表示。"
+    override fun getRequirements(): String = """
+1. 如果存在相互引用，请用红线展示出来。
+2. 只展示重要的分层，不要展示过多的细节。
+4. 结合分层、subgraph 的方式来表示分层。
+4. 示例如下：
+
+```dot
+digraph G {
+    rankdir=TB;
+    node [shape=record, fontname=Helvetica];
+    edge [color=black, penwidth=1.0];
+    subgraph cluster_{} {
+        label="{} Layer"
+    }
+```
+"""
 
     override fun getExtendData(): String {
         val codeContext = ComateSourceCodeContext.create(context.workdir.toString(), context.lang)
