4fa52b21727d5ebf71a935795cfc634f6a5a3f07
Phodal Huang <h@phodal.com>
2023-06-16 17:47:14 +0800

    fix: fix test

--- comate-cli/src/test/kotlin/org/archguard/comate/dynamic/ContextFunctionTest.kt
+++ comate-cli/src/test/kotlin/org/archguard/comate/dynamic/ContextFunctionTest.kt
@@ -8,8 +8,8 @@ class ContextFunctionTest {
     fun should_read_all_context_functions() {
         val contextFunction = ContextFunction()
         val functions = contextFunction.functions()
-        assertEquals(1, functions.size)
+        assertEquals(2, functions.size)
 
-        assertEquals(functions[0], "analysis_system")
+        assertEquals(functions.contains("analysis_system"), true)
     }
 }
