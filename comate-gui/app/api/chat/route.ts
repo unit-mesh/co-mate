03f7b97c5dd85e0cbe08de3f4a8fd67179de2bde
Phodal Huang <h@phodal.com>
2023-06-19 10:09:17 +0800

    chore: reinit gui from https://github.com/vercel-labs/ai-chatbot

---
+++ comate-gui/app/api/chat/route.ts
@@ -0,0 +1,66 @@
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
+import { Configuration, OpenAIApi } from 'openai-edge'
+
+import { auth } from '@/auth'
+import { nanoid } from '@/lib/utils'
+
+export const runtime = 'edge'
+
+export async function POST(req: Request) {
+  const json = await req.json()
+  const { messages, previewToken } = json
+  const session = await auth()
+
+  if (process.env.VERCEL_ENV !== 'preview') {
+    if (session == null) {
+      return new Response('Unauthorized', { status: 401 })
+    }
+  }
+
+  const configuration = new Configuration({
+    apiKey: previewToken || process.env.OPENAI_API_KEY
+  })
+
+  const openai = new OpenAIApi(configuration)
+
+  const res = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages,
+    temperature: 0.7,
+    stream: true
+  })
+
+  const stream = OpenAIStream(res, {
+    async onCompletion(completion) {
+      const title = json.messages[0].content.substring(0, 100)
+      const userId = session?.user.id
+      if (userId) {
+        const id = json.id ?? nanoid()
+        const createdAt = Date.now()
+        const path = `/chat/${id}`
+        const payload = {
+          id,
+          title,
+          userId,
+          createdAt,
+          path,
+          messages: [
+            ...messages,
+            {
+              content: completion,
+              role: 'assistant'
+            }
+          ]
+        }
+        await kv.hmset(`chat:${id}`, payload)
+        await kv.zadd(`user:chat:${userId}`, {
+          score: createdAt,
+          member: `chat:${id}`
+        })
+      }
+    }
+  })
+
+  return new StreamingTextResponse(stream)
+}
---
+++ comate-gui/components/header.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react'
+import Link from 'next/link'
+
+import { cn } from '@/lib/utils'
+import { auth } from '@/auth'
+import { clearChats } from '@/app/actions'
+import { buttonVariants } from '@/components/ui/button'
+import { Sidebar } from '@/components/sidebar'
+import { SidebarList } from '@/components/sidebar-list'
+import {
+  IconGitHub,
+  IconNextChat,
+  IconSeparator,
+  IconVercel
+} from '@/components/ui/icons'
+import { SidebarFooter } from '@/components/sidebar-footer'
+import { ThemeToggle } from '@/components/theme-toggle'
+import { ClearHistory } from '@/components/clear-history'
+import { UserMenu } from '@/components/user-menu'
+import { LoginButton } from '@/components/login-button'
+
+export async function Header() {
+  const session = await auth()
+  return (
+    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl">
+      <div className="flex items-center">
+        {session?.user ? (
+          <Sidebar>
+            <React.Suspense fallback={<div className="flex-1 overflow-auto" />}>
+              {/* @ts-ignore */}
+              <SidebarList userId={session?.user?.id} />
+            </React.Suspense>
+            <SidebarFooter>
+              <ThemeToggle />
+              <ClearHistory clearChats={clearChats} />
+            </SidebarFooter>
+          </Sidebar>
+        ) : (
+          <Link href="/" target="_blank" rel="nofollow">
+            <IconNextChat className="mr-2 h-6 w-6 dark:hidden" inverted />
+            <IconNextChat className="mr-2 hidden h-6 w-6 dark:block" />
+          </Link>
+        )}
+        <div className="flex items-center">
+          <IconSeparator className="h-6 w-6 text-muted-foreground/50" />
+          {session?.user ? (
+            <UserMenu user={session.user} />
+          ) : (
+            <LoginButton
+              variant="link"
+              showGithubIcon={false}
+              text="Login"
+              className="-ml-2"
+            />
+          )}
+        </div>
+      </div>
+      <div className="flex items-center justify-end space-x-2">
+        <a
+          target="_blank"
+          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          rel="noopener noreferrer"
+          className={cn(buttonVariants({ variant: 'outline' }))}
+        >
+          <IconGitHub />
+          <span className="ml-2 hidden md:flex">GitHub</span>
+        </a>
+        <a
+          href="https://github.com/vercel/nextjs-ai-chatbot/"
+          target="_blank"
+          className={cn(buttonVariants())}
+        >
+          <IconVercel className="mr-2" />
+          <span className="hidden sm:block">Deploy to Vercel</span>
+          <span className="sm:hidden">Deploy</span>
+        </a>
+      </div>
+    </header>
+  )
+}
