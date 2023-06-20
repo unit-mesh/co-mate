4fc5c06fa14771391efa22146cf1ca41d6cb287b
Phodal Huang <h@phodal.com>
2023-06-20 10:10:15 +0800

    docs: update todos

--- comate-gui/components/chat-message.tsx
+++ comate-gui/components/chat-message.tsx
@@ -18,13 +18,44 @@ export interface ToolingThought {
   actionInput: string
 }
 
-function renderMessage(message: Message) {
-  // todo: parse for Thought Messages
-  // Thought: I need to introduce the system to the team and ensure that it aligns with our overall architecture and governance policies.
-  // Action: introduce_system
-  // Action Input: https://github.com/archguard/ddd-monolithic-code-sample
-  console.log(message.content)
 
+// Thought: I need to introduce the system to the team and ensure that it aligns with our overall architecture and governance policies.
+// Action: introduce_system
+// Action Input: https://github.com/archguard/ddd-monolithic-code-sample
+const thoughtRegex = /\s*Thought:\s*(.*(?:\n(?!\s*\/\/).*)*)/i;
+const actionRegex = /\s*Action:\s*(.*(?:\n(?!\s*\/\/).*)*)/i;
+const actionInputRegex = /\s*Action\s*Input:\s*(.*(?:\n(?!\s*\/\/).*)*)/i;
+
+function renderMessage(message: Message) {
+
+  let content = message.content;
+
+  let splitContent = content.split('\n');
+  let firstLine = splitContent[0];
+
+  console.log(thoughtRegex.test(firstLine));
+  if (splitContent.length >= 1 && thoughtRegex.test(firstLine)) {
+    let thought = thoughtRegex.exec(firstLine)?.[1] ?? "";
+    let action = ""
+    if (splitContent.length >= 2) {
+      action = actionRegex.exec(splitContent[1])?.[1] ?? "";
+    }
+
+    let actionInput = ""
+    if (splitContent.length >= 3) {
+      actionInput = actionInputRegex.exec(splitContent[2])?.[1] ?? "";
+    }
+
+    let toolingThought: ToolingThought = {
+      thought: thought,
+      action: action,
+      actionInput: actionInput
+    }
+
+    content = `${thought}\n | Action | Action Input |\n | ------ | ------------ |\n | ${action} | ${actionInput} |`;
+  }
+
+  console.log(content)
 
   return <MemoizedReactMarkdown
     className="prose whitespace-pre-wrap break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
@@ -65,7 +96,7 @@ function renderMessage(message: Message) {
       }
     }}
   >
-    {message.content}
+    {content}
   </MemoizedReactMarkdown>;
 }
 
