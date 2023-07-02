a922a0d42f9f25b58b50f01d2ebf93e964f65879
Phodal Huang <h@phodal.com>
2023-07-02 22:07:00 +0800

    feat: update for concept spec

--- spec-lang/src/main/kotlin/org/archguard/spec/lang/ObjectSpec.kt
+++ spec-lang/src/main/kotlin/org/archguard/spec/lang/ObjectSpec.kt
@@ -1,6 +1,58 @@
 package org.archguard.spec.lang
 
+import kotlinx.serialization.Serializable
 import org.archguard.spec.lang.base.Spec
+
+@Serializable
+data class Class(
+    val name: String,
+    val properties: MutableList<Property> = mutableListOf(),
+    val methods: MutableList<Method> = mutableListOf(),
+)
+
+@Serializable
+data class Property(val name: String, val type: String)
+
+@Serializable
+data class Method(val name: String, val returnType: String, val parameters: List<Parameter> = emptyList())
+
+@Serializable
+data class Parameter(val name: String, val type: String)
+
+class ClassSpec(private val className: String) {
+    private val properties = mutableListOf<Property>()
+    private val methods = mutableListOf<Method>()
+
+    fun prop(name: String, type: String) {
+        properties.add(Property(name, type))
+    }
+
+    fun method(name: String, returnType: String, block: MethodBuilder.() -> Unit) {
+        val methodBuilder = MethodBuilder(name, returnType)
+        methodBuilder.block()
+        methods.add(methodBuilder.build())
+    }
+
+    fun build(): Class {
+        return Class(className, properties, methods)
+    }
+
+    fun prop(name: Pair<String, String>) {
+        properties.add(Property(name.first, name.second))
+    }
+
+    inner class MethodBuilder(private val name: String, private val returnType: String) {
+        private val parameters = mutableListOf<Parameter>()
+
+        fun parameter(name: String, type: String) {
+            parameters.add(Parameter(name, type))
+        }
+
+        fun build(): Method {
+            return Method(name, returnType, parameters)
+        }
+    }
+}
 
 class ConceptSpec : Spec<String> {
     override fun default(): Spec<String> {
@@ -13,15 +65,27 @@ class ConceptSpec : Spec<String> {
 
     companion object {
         fun defaultSpec(): ConceptSpec {
-            return concept {
-//                concepts {}
-//                relations {}
+            return concepts {
+                concept("Person") {
+                    prop("name" to "String")
+                    prop("age", "Int")
+
+                    method("speak", "Unit") {
+                        parameter("message", "String")
+                    }
+                }
             }
         }
     }
+
+    fun concept(className: String, function: ClassSpec.() -> Unit): ClassSpec {
+        val classSpec = ClassSpec(className)
+        classSpec.function()
+        return classSpec
+    }
 }
 
-fun concept(function: ConceptSpec.() -> Unit): ConceptSpec {
+fun concepts(function: ConceptSpec.() -> Unit): ConceptSpec {
     val conceptSpec = ConceptSpec()
     conceptSpec.function()
     return conceptSpec
--- spec-lang/src/test/kotlin/org/archguard/spec/lang/ConceptSpecTest.kt
+++ spec-lang/src/test/kotlin/org/archguard/spec/lang/ConceptSpecTest.kt
@@ -1,10 +1,11 @@
 package org.archguard.spec.lang
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
 class ConceptSpecTest {
-    @org.junit.jupiter.api.Test
-    fun testDefaultSpec() {
+    @Test
+    fun should_equal_when_had_same_spec() {
         val spec = ConceptSpec.defaultSpec()
         assertEquals(ConceptSpec.defaultSpec().toString(), spec.toString())
     }
