218b4428b2d6fbdcb04149b6c0656e17108bcac6
Phodal Huang <h@phodal.com>
2023-07-06 08:32:00 +0800

    docs: update for cases

--- spec-lang/src/main/kotlin/org/archguard/spec/lang/ConceptSpec.kt
+++ spec-lang/src/main/kotlin/org/archguard/spec/lang/ConceptSpec.kt
@@ -18,26 +18,51 @@ class ConceptSpec : Spec<String> {
     override fun toString(): String = "concept { ${concepts.joinToString(", ")} }"
 
     companion object {
-        fun defaultSpec(): ConceptSpec {
-            return concepts {
+        fun defaultSpec(): ConceptSpec =
+            concepts {
                 val customer = Concept("Customer") {
                     behavior("Place Order", "Place an order for a coffee")
-                    behaviors = listOf("View Menu", "Place Order", "Pay", "View Order Status", "View Order History")
+                    behaviors = listOf(
+                        "View Menu",
+                        "Add to Cart",
+                        "Remove from Cart",
+                        "Place Order",
+                        "Pay",
+                        "View Order Status",
+                        "View Order History",
+                        "Customize Order"
+                    )
                 }
 
                 val barista = Concept("Barista") {
                     behavior("Make Coffee")
                 }
 
+                val deliveryPerson = Concept("Delivery Person") {
+                    behavior("Deliver Order")
+                }
+
+                val shoppingCart = Concept("Shopping Cart") {
+                    behavior("Add to Cart", "Add a coffee to the shopping cart")
+                    behavior("Remove from Cart", "Remove a coffee from the shopping cart")
+                    behavior("View Cart", "View the contents of the shopping cart")
+                    behavior("Checkout", "Proceed to checkout and place the order")
+                }
+
                 relations {
-                    customer["Place Order"] perform barista
                     customer["View Menu"] perform barista
                     customer["View Order History"] perform barista
 
-                    customer["Custom something"].perform(barista)
+                    customer["Add to Cart"] perform shoppingCart
+                    customer["Remove from Cart"] perform shoppingCart
+                    customer["View Cart"] perform shoppingCart
+                    customer["Checkout"] perform barista
+                    customer["Place Order"] perform barista
+                    customer["Pay"] perform deliveryPerson
+                    customer["View Order Status"] perform deliveryPerson
+                    customer["Customize Order"].perform(barista)
                 }
             }
-        }
     }
 
     fun relations(functions: CodeBlock.() -> Unit) = functions()
